c97000026835edf2ca8f80ae670cf1fa2878dc41
Attraya Das <attrayaghoshdas@gmail.com>
2024-01-16 19:55:08 +0530

    Edit controller to publish 1L messages to topic

--- src/main/java/com/attraya/controller/PaytmController.java
+++ src/main/java/com/attraya/controller/PaytmController.java
@@ -2,6 +2,7 @@ package com.attraya.controller;
 
 import com.attraya.dto.PaymentRequest;
 import com.attraya.dto.PaytmRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -27,11 +28,16 @@ public class PaytmController {
     }
 
     @PostMapping("/paytm/payment")
-    public String doPayment(@RequestBody PaytmRequest<PaymentRequest> paytmRequest){
-        PaymentRequest paymentRequest = paytmRequest.getPayload();
-        paymentRequest.setTransactionId(UUID.randomUUID().toString());
-        paymentRequest.setTransactionDate(new Date());
-        kafkaTemplate.send(topicName, paytmRequest);
+    public String doPayment(@RequestBody PaytmRequest<PaymentRequest> paytmRequest) throws JsonProcessingException {
+        for(int i =0; i<100000; i++) {
+            PaymentRequest paymentRequest = paytmRequest.getPayload();
+            paymentRequest.setSourceAccount("SRC_ACC"+i);
+            paymentRequest.setDestinationAccount("DESTN_ACC"+i);
+            paymentRequest.setAmount(500+i);
+            paymentRequest.setTransactionId(UUID.randomUUID().toString());
+            paymentRequest.setTransactionDate(new Date());
+            kafkaTemplate.send(topicName, paymentRequest); // payment-topic1 has 3 partition
+        }
         return "Payment Instantiated Successfully :)";
     }
 }
